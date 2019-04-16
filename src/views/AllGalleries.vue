4871b28b1ef37ebdd0dea0f3bf79fdac97527d80
BokiGaja <bgajic93@gmail.com>
2019-04-16 14:29:28 +0200

    Pagination added

--- src/views/AllGalleries.vue
+++ src/views/AllGalleries.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
-    <div v-if="getGalleries.length !== 0">
-      <div v-for="(gallery, index) in getGalleries" :key="index">
+    <div v-if="getGalleries.length !== 0" style="text-align: center">
+      <div v-for="(gallery, index) in getGalleries.slice(0, currPage*10)" :key="index">
         <app-gallery-card :gallery="gallery" :singleGallery="false"/>
       </div>
+      <button class="btn btn-info btn-lg" @click="currPage++" v-if="getGalleries.length > currPage*10">Load more
+      </button>
     </div>
     <div class="alert alert-danger" v-if="getGalleries.length === 0" style="text-align: center">There is no gallery
     </div>
@@ -19,11 +21,16 @@
     components: {
       appGalleryCard: GalleryCard
     },
+    data() {
+      return {
+        currPage: 1
+      }
+    },
     computed: {
       ...mapGetters(['getGalleries'])
     },
     watch: {
-      '$route'(to, from) {
+      '$route'() {
         this.$router.go(this.$router.currentRoute)
       }
     },
