13449afe3ac2332b5bb8067e896a5b9362b22d0f
BokiGaja <bgajic93@gmail.com>
2019-04-17 14:15:53 +0200

    Carousel added

--- src/components/AppGalleryCard.vue
+++ src/components/AppGalleryCard.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="card galleryCard">
+  <div :class="singleGallery? 'card singleGalleryCard' : 'card galleryCard'">
     <div class="card-body">
       <h5 class="card-title cardLink" @click="seeGallery">{{gallery.title}}</h5>
-      <!--    <img class="card-img-top" src="..." alt="Card image cap">-->
-      <router-link v-if="singleGallery" :to="{name: 'authorsGallery', params:{id: this.gallery.user.id}}" class="cardLink"> {{gallery.user.first_name}} {{gallery.user.last_name}}</router-link>
+      <img class="card-img-top" :src="gallery.pictures[0].imageUrl" alt="Card image cap" v-if="!singleGallery">
+      <pictures-carousel v-if="singleGallery" :pictures="gallery.pictures" style="width: 600px; height: 400px"/>
+      <router-link v-if="singleGallery" :to="{name: 'authorsGallery', params:{id: this.gallery.user.id}}"
+                   class="cardLink"> {{gallery.user.first_name}} {{gallery.user.last_name}}
+      </router-link>
       <p v-if="singleGallery"> Created at: {{gallery.created_at | formatDate}}</p>
       <p class="card-text" v-if="singleGallery">{{gallery.description}}</p>
     </div>
     <div class="card-footer text-muted" v-if="!singleGallery">
-      Created by:<router-link :to="{name: 'authorsGallery', params:{id: this.gallery.user.id}}" class="cardLink"> {{gallery.user.first_name}} {{gallery.user.last_name}}</router-link>
+      Created by:
+      <router-link :to="{name: 'authorsGallery', params:{id: this.gallery.user.id}}" class="cardLink">
+        {{gallery.user.first_name}} {{gallery.user.last_name}}
+      </router-link>
       <br>
       {{gallery.created_at | formatDate}}
     </div>
@@ -16,17 +22,21 @@
 </template>
 
 <script>
-  import {formatDate} from "../mixins/DateMixin";
+  import {formatDate} from "../mixins/DateMixin"
+  import PicturesCarousel from '@/components/PicturesCarousel'
 
   export default {
     name: "GalleryCard",
+    components: {
+      PicturesCarousel
+    },
     props: {
       gallery: Object,
       singleGallery: Boolean
     },
     methods: {
       seeGallery() {
-        this.$router.push('/galleries/'+ this.gallery.id)
+        this.$router.push('/galleries/' + this.gallery.id)
       },
       seeAuthor() {
         this.$router.push();
@@ -42,6 +52,13 @@
     margin: 20px auto;
     text-align: center
   }
+
+  .singleGalleryCard {
+    width: 800px;
+    text-align: center;
+    margin: auto;
+  }
+
   .cardLink {
     cursor: pointer;
   }
---
+++ src/components/PicturesCarousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin: auto">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" v-for="(picture, index) in pictures" :data-slide-to="index"
+          :class="index === 0 ? 'active' : null" :key="index"/>
+    </ol>
+    <div class="carousel-inner">
+      <div :class="index === 0 ? 'carousel-item active' : 'carousel-item'" v-for="(picture, index) in pictures"
+           :key="index">
+        <img class="d-block carousel-image" :src="picture.imageUrl" alt="First slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PicturesCarousel",
+    props: ['pictures']
+  }
+</script>
+
+<style scoped>
+  .carousel-image {
+    width: 600px;
+    height: 400px;
+  }
+</style>
