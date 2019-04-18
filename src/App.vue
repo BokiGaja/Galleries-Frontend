a3c8c5a6ecc2cb4beee35cf7bd686f4c4c5b8999
BokiGaja <bgajic93@gmail.com>
2019-04-18 16:36:54 +0200

    Transition added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <app-navigation/>
-    <router-view/>
+    <transition name="slide" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -32,5 +34,37 @@
     background-position: top;
     background-repeat: no-repeat;
     background-size: cover;
+  }
+  .slide-leave-active {
+    transition: opacity 1s ease;
+    opacity: 0;
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  .slide-leave {
+    opacity: 1;
+    transform: translateX(0);
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+
+  @keyframes slide-out {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-30px);
+    }
+  }
+
+  @keyframes slide-in {
+    0% {
+      transform: translateY(-30px);
+    }
+    100% {
+      transform: translateY(0);
+    }
   }
 </style>
